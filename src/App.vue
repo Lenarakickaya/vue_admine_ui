7adf28bafa8a57d40255729d81eb5b3f1a4119d6
Lena <mazaniklena9@gmail.com>
2023-12-21 20:35:43 +0300

    first

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <HeaderComponent></HeaderComponent>
+    <router-view></router-view>
+    <!--<HelloWorld msg="Welcome to Your Vue.js App" />-->
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/layout/HeaderComponent";
+//import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    HeaderComponent,
+    //HelloWorld,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
